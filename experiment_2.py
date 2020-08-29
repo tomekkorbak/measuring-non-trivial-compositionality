c8c6f62928ecd4962f007f2bf47624701c529ce3
Tomek Korbak <tomasz.korbak@gmail.com>
2020-08-29 19:53:29 +0200

    Update experiment 2

--- experiment_2.py
+++ experiment_2.py
@@ -15,7 +15,7 @@ from protocols import get_negation_ntc_protocol, get_context_sensitive_ntc_proto
 sns.set_style("white")
 NUM_COLORS = NUM_SHAPES = 10
 NUM_SEEDS = 3
-df = pd.DataFrame(columns=['protocol', 'metric', 'value', 'seed'])
+df = pd.DataFrame(columns=['protocol', 'metric', 'value', 'seed', 'wd'])
 neptune.init('tomekkorbak/ntc')
 neptune.create_experiment(upload_source_files=['**/*.py*'], properties=dict(num_seeds=NUM_SEEDS, num_colors=NUM_COLORS))
 protocol = namedtuple('Protocol', ['protocol_name', 'protocol_obj', 'max_length', 'num_concepts'])
@@ -38,15 +38,15 @@ for seed in range(NUM_SEEDS):
             for metric_name, metric in metrics.items():
                 print(protocol_name, metric_name)
                 value = metric.measure(protocol_obj)
-                df.loc[len(df)] = [protocol_name, metric_name, -value, seed]
+                df.loc[len(df)] = [protocol_name, metric_name, -value, seed, wd]
 log_table('df', df)
 df.to_csv('results_2.csv')
 
 
 with sns.plotting_context('paper', font_scale = 1.3):
     all_tre = df[df['protocol'] != 'holistic']
-    p = sns.catplot(x='value', y='protocol', col='metric', data=df, kind='box',
-                sharex=True, col_wrap=4, height=2.5, margin_titles=True)
+    p = sns.catplot(x='value', y='protocol', col='metric', row='wd', data=df, kind='box',
+                    sharex=True, height=2.5, margin_titles=True)
     p.set_titles(row_template='{row_name}', col_template = '{col_name}')
     p.savefig('figure_1.png')
     neptune.log_image('figure', 'figure_1.png')
